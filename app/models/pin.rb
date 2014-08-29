b0be6b25c00fff2d43ba3b79a22730a9f3315fb4
kvanadev <kvanadev1@gmail.com>
2014-08-29 14:31:38 +0530

    image Uploaded successfully

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,9 @@
 class Pin < ActiveRecord::Base
 
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100"}
+	has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100"}, :default_url => "/imaes/:styles/missing.png"
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+	validates :description, presence: true
+    validates :image, presence:true
 end
