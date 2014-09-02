fa566625cf462af48fe534c723d8ba7722d76b1f
kvanadev <kvanadev1@gmail.com>
2014-09-02 21:48:13 +0530

    Add Amazon S3 for Paperclip uploads to Heroku

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100"}, :default_url => "/imaes/:styles/missing.png"
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/imaes/:styles/missing.png"
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 	validates :description, presence: true
